9825bba01a9c8543ac53d4f4bb279215b44e1f24
Wei Fan <weifan@taiyiyun.com>
2017-06-07 18:54:50 +0800

    add protos

--- crypto/privkey.go
+++ crypto/privkey.go
@@ -12,19 +12,47 @@ import (
 )
 
 type ECDSAPriv struct {
-	curveType asn1.Enumerated
+	CurveType asn1.Enumerated "tag:10"
 	D *big.Int
 }
 
-func DumpPrivKey(priv ECDSAPriv) (string, error){
-	
+func dumpPrivKey(priv ECDSAPriv) ([]byte, error){
+
 	rbyte, err := asn1.Marshal(priv)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	
-	return base64.StdEncoding.EncodeToString(rbyte), nil
+	return rbyte, nil
+}
+
+func importPrivKey(kb []byte)(*ECDSAPriv, error){
 	
+	var priv = ECDSAPriv{}
+	_, err := asn1.Unmarshal(kb, &priv)
+	
+	if err != nil {
+		return nil, err
+	}
+	
+	return &priv, nil		
+}
+
+const(
+	
+	ECP256_FIPS186 = 1
+	ECP256_SEC2k1 = 16
+)
+
+
+func DumpPrivKey(priv ECDSAPriv) (string, error){
+	
+	rb, err := dumpPrivKey(priv)
+	if err != nil{
+		return "ERROR", err
+	}
+	
+	return base64.StdEncoding.EncodeToString(rb), nil
 }
 
 func PrivKeyfromString(kstr string) (*ECDSAPriv, error){
@@ -34,25 +62,22 @@ func PrivKeyfromString(kstr string) (*ECDSAPriv, error){
 		return nil, err
 	}
 	
-	var priv ECDSAPriv
-	_, err = asn1.Unmarshal(data, priv)
-	
-	if err != nil {
-		return nil, err
-	}
-	
-	return &priv, nil		
+	return importPrivKey(data)	
 }
 
 func (k ECDSAPriv) apply() (*ecdsa.PrivateKey, error){
 	
 	var curve elliptic.Curve	
 	
-	switch(k.curveType){
+	if k.D == nil{
+		return nil, errors.New(fmt.Sprintf("empty seed field"))	
+	}
+	
+	switch(k.CurveType){
 		case 1:
 			curve = elliptic.P256()
 		default:
-			return nil, errors.New(fmt.Sprintf("%d is not a valid curve defination", k.curveType))		
+			return nil, errors.New(fmt.Sprintf("%d is not a valid curve defination", k.CurveType))		
 	}
 	
 	retx, rety := curve.ScalarBaseMult(k.D.Bytes())
--- crypto/privkey_test.go
+++ crypto/privkey_test.go
@@ -15,7 +15,13 @@ func TestApply_Privkey(t *testing.T){
 		t.Skip("Skip for ecdsa lib fail:", err)
 	}
 	
-	var prvkeyt = ECDSAPriv{1, prvkeystd.D}
+	rb := make([]byte, 32)
+	_, err = rand.Read(rb)
+	if err != nil{
+		t.Skip("rand make 256bit bytes fail", err)
+	}	
+	
+	var prvkeyt = ECDSAPriv{ECP256_FIPS186, prvkeystd.D}
 	prvkeytapp, err := prvkeyt.apply()
 	
 	if err != nil{
@@ -26,4 +32,46 @@ func TestApply_Privkey(t *testing.T){
 		t.Fatal("Unmatch public key:", prvkeytapp.X.Text(16), prvkeystd.X.Text(16), 
 			prvkeytapp.Y.Text(16), prvkeystd.Y.Text(16))
 	}
+	
+	sx, sy, err := ecdsa.Sign(rand.Reader, prvkeytapp, rb)
+	if err != nil{
+		t.Fatal(err)
+	}
+	
+	if !ecdsa.Verify(&prvkeystd.PublicKey, rb, sx, sy) {
+		t.Fatal("verify private key fail")
+	}
+}
+
+func TestDump_Privkey(t *testing.T){
+	
+	prvkeystd, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil{
+		t.Skip("Skip for ecdsa lib fail:", err)
+	}
+	
+	prvkeyt := ECDSAPriv{1, prvkeystd.D}
+	
+	dmpstr, err := DumpPrivKey(prvkeyt)
+	
+	if err != nil{
+		t.Fatal(err)
+	}
+	
+	t.Log("dump privkey as", dmpstr)
+	
+	prvkeyresume, err := PrivKeyfromString(dmpstr)
+	
+	if err != nil{
+		t.Fatal(err)
+	}
+	
+	if err != nil{
+		t.Fatal(err)
+	}
+	
+	if prvkeyresume.D.Cmp(prvkeystd.D) != 0 || prvkeyresume.CurveType != ECP256_FIPS186{
+		t.Fatal("Unmatch private key number:", prvkeyresume.D.Text(16), prvkeystd.D.Text(16))		
+	}
+		
 }
