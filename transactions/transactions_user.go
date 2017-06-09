cc062a4a6ed117c7f6e70ea4e06b64bb69603ccc
weifan <noel.wei@gmail.com>
2017-06-09 09:46:20 +0800

    transactions

--- transactions/transactions_user.go
+++ transactions/transactions_user.go
@@ -3,23 +3,33 @@ package transactions
 import (
 	"fmt"
 	"errors"
-	_ "crypto/ecdsa"
+	"crypto/ecdsa"
 	"math/big"
 	"encoding/base64"
 	
 	"github.com/golang/protobuf/proto"
 	pb "gamecenter.mobi/paicode/protos"
-	_ "gamecenter.mobi/paicode/crypto"
+
 )
 
-type FundTx struct{
-	Nounce []byte
+type FundTxData struct{
 	From string
 	To   string
-	Amount uint
+	Amount uint	
+}
+
+type FundTx struct{
+	FundTxData
+	Nounce []byte
 	Invoked bool
 	InvokedCode uint
 	signX, signY *big.Int
+}
+
+func (f *FundTxData) fill(v *pb.Funddata) {
+	
+	f.Amount = uint(v.Pai)
+	f.To = v.ToUserId
 }
 
 func (f *FundTx) fill(v interface{}) error{
@@ -28,9 +38,58 @@ func (f *FundTx) fill(v interface{}) error{
 		f.From = data.FundId
 		f.Nounce = data.Nounce
 		case *pb.Fund:
+		switch inndata := data.D.(type){
+			case *pb.Fund_Userfund:
+			f.FundTxData.fill(inndata.Userfund)
+			f.Invoked = false
+			case *pb.Fund_InvokeChaincode:
+			f.Invoked = true
+			f.InvokedCode = uint(inndata.InvokeChaincode)
+			default:
+			return errors.New(fmt.Sprint("encounter unexpected type in fund field as %T", inndata))
+		}
 		case *pb.Signature:
+		f.signX = big.NewInt(0).SetBytes(data.P.X)
+		f.signY = big.NewInt(0).SetBytes(data.P.Y)
 		case *pb.Funddata:
+		f.FundTxData.fill(data)
 	}
+	
+	return nil
+}
+
+func (f *FundTx) MakeTransaction() (args []string, err error){
+	args = make([]string, 3, 4)
+	err = nil
+		
+	field1 := &pb.UserTxHeader{FundId: f.From, Nounce: f.Nounce}
+	field2 := &pb.Fund{}
+	field3 := &pb.Signature{&pb.ECPoint{f.signX.Bytes(), f.signY.Bytes()}}	
+	field4 := &pb.Funddata{Pai: uint32(f.Amount), ToUserId: f.To}
+	
+	fields := append(make([]proto.Message, 0, 4), field1, field2, field3)
+	
+	if f.Invoked {
+		field2.D = &pb.Fund_InvokeChaincode{uint32(f.InvokedCode)}		
+		fields = append(fields, field4)
+		args = append(args, "")
+	}else{
+		field2.D = &pb.Fund_Userfund{field4}
+	}
+		
+	for i, field := range fields{
+		rb, errx := proto.Marshal(field)
+		if errx != nil{
+			err = errx
+			return
+		}		
+		args[i] = base64.StdEncoding.EncodeToString(rb)
+	}
+	
+	return
+}
+
+func (f *FundTx) SignTransaction(ecdsa.PrivateKey) error{
 	
 	return nil
 }
@@ -40,6 +99,8 @@ func ParseFundTransaction(args []string) (*FundTx, error){
 	if len(args) < 3 {
 		return nil, errors.New(fmt.Sprint("Not enough args, expect at least 3 but only", len(args)))
 	}
+	
+	var pargs []string
 	
 	ftx := new(FundTx)
 	
@@ -54,10 +115,12 @@ func ParseFundTransaction(args []string) (*FundTx, error){
 		switch i {
 			case 0:vif = &pb.UserTxHeader{}
 			case 1:vif = &pb.Fund{}
-			case 2:vif = &pb.Signature{}
+			case 2:
+				if ftx.Invoked && len(pargs) == 3{
+					return nil, errors.New("Miss field for invoked transaction")
+				}
+				vif = &pb.Signature{}
 			case 3:vif = &pb.Funddata{}
-			default:
-				
 		}
 		
 		err = proto.Unmarshal(data, vif)
@@ -68,12 +131,15 @@ func ParseFundTransaction(args []string) (*FundTx, error){
 		err = ftx.fill(vif)
 		if err != nil{
 			return nil, errors.New(fmt.Sprint("filling transaction fail", err))
+		}
+		
+		if i == 2 && !ftx.Invoked{
+			//done
+			break
 		}		
-	}
+	}	
 	
-	
-	
-	return nil, nil
+	return ftx, nil
 }
 
 
